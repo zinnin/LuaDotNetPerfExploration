8f7582f6c6c7054c8932cbf27a62e75a18d7e94e
zinnin <34364024+zinnin@users.noreply.github.com>
2021-09-07 23:28:54 -0700

    Initial Structure

---
+++ PerfTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerfTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
